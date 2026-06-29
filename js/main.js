22eef9d86258c6f0b99a09cca0a1d230b98dfe98
Sumit Kumar Das <skdas5405@gmail.com>
2026-06-29 19:07:27 +0530

    Rebuild Promise Centre for Education on the KidKinder theme with Promise branding
    New design (KidKinder template) recolored to the Promise logo palette (maroon #790002 / gold #996A01 / navy #393D72), with the Promise crest logo, Promise navigation and the client's content across all pages: Home, About (vision & mission, founder's message, infrastructure, faculty), Courses (programmes, weekly schedule, fees), Results, Gallery, Blog, Contact. Removed template credits, mail scripts and unused source. Names/contact details remain bracketed placeholders pending client data.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 (function ($) {
     "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
             }
-        }, 1);
-    };
-    spinner(0);
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
     
     
-    // Initiate the wowjs
-    new WOW().init();
-    
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
@@ -30,16 +33,31 @@
     });
 
 
-    // Testimonial carousel
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
 
-    $(".testimonial-carousel").owlCarousel({
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Post carousel
+    $(".post-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1500,
-        center: true,
-        dots: true,
+        dots: false,
         loop: true,
-        margin: 50,
-        responsiveClass: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
         responsive: {
             0:{
                 items:1
@@ -52,32 +70,33 @@
             },
             992:{
                 items:2
-            },
-            1200:{
-                items:3
             }
         }
     });
 
 
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        smartSpeed: 2000,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
     });
-
-
-
+    
 })(jQuery);
 
